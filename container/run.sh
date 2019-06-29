540d48c0e7656047e4369c215c2dc114e42e3bde
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-06-29 20:39:27 +0200

    Make a cosmetic adjustment

--- container/run.sh
+++ container/run.sh
@@ -40,7 +40,7 @@ function process_application() {
     --config configs/${ACTION}.json
   # Copy the result to the output location in Cloud Storage
   save output ${output}
-  # Define the input location in Cloud Storage
+  # Define the input file in Cloud Storage
   local input=${output}/predictions.csv
   # Define the output table in BigQuery
   local output=$(echo ${NAME} | tr - _).predictions
