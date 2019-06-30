6a5c47721ebc0081d8bb357d9e2dff29286e7a27
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-06-30 16:19:02 +0200

    Annotate the graph

--- scheduler/graph.py
+++ scheduler/graph.py
@@ -23,12 +23,15 @@ def configure():
             return value
         return value
 
+    # Extract the directory containing the current file
     path = os.path.dirname(__file__)
+    # Extract the name of the current file without the extension
     name = os.path.splitext(os.path.basename(__file__))[0]
+    # Load the configuration file corresponding to the extracted name
     config = os.path.join(path, 'configs', name + '.json')
-    if os.path.exists(config):
-        config = json.loads(open(config).read())
-        return _parameterize(config, {'${ROOT}': path})
+    config = json.loads(open(config).read())
+    # Replace all occurrences of “${ROOT}” with the current directory
+    return _parameterize(config, {'${ROOT}': path})
 
 
 def construct(config):
@@ -40,16 +43,21 @@ def construct(config):
     def _construct_task(graph, name, code):
         return BashOperator(task_id=name, bash_command=code, dag=graph)
 
+    # Construct an empty graph
     graph = _construct_graph(**config['airflow'])
+    # Construct tasks according to the configuration file
     tasks = [_construct_task(graph, **task) for task in config['tasks']]
     tasks = dict([(task.task_id, task) for task in tasks])
+    # Enforce dependencies between tasks according to the configuration file
     for child, parent in config['dependencies']:
         tasks[parent].set_downstream(tasks[child])
     return graph
 
 
-config = configure()
-if config is not None:
-    graph = construct(config)
+try:
+    graph = construct(configure())
     if __name__ == '__main__':
         graph.cli()
+except FileNotFoundError:
+    # Exit without errors in case the current file has no configuration file
+    pass
